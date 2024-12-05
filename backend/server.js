ab084be9b9b78223eb53bc54109d61b328d4ec00
Umar-Mukthar-Ahmed <21j56.umar@sjec.ac.in>
2024-12-05 20:57:40 +0530

    feat: added cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
-const express= require ('express');
-const {PORT} = require ('./config/index');
+const express = require('express');
+const { PORT } = require('./config/index');
 const dbConnect = require('./database/index');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/errorHandler');
-const app=express();
+const cookieParser = require('cookie-parser');
 
+const app = express();
+
+app.use(cookieParser());
 
 app.use(express.json());
+
 app.use(router);
+
 dbConnect();
 
 app.use(errorHandler);
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`the app is running on port http://localhost:${PORT}`);
 });
