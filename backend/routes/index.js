f6b6b84e87df98436da2bb746d67a98dd955bd41
Umar-Mukthar-Ahmed <21j56.umar@sjec.ac.in>
2024-12-08 08:34:49 +0530

    feat: add routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 const authController = require('../controller/authController');
+const blogController = require('../controller/blogController');
+const commentController = require('../controller/commentController');
 
 
 //register
-router.post('/register',authController.register);
+router.post('/register', authController.register);
 //login
 router.post('/login', authController.login);
+
+//logout
+router.post('/logout', auth, authController.logout);
+
+//refresh
+router.get('/refresh', authController.refresh);
+
+
+
+//blog operations
+//create blog
+router.post('/blog', blogController.create);
+
+//getall
+router.get('/blog/all', blogController.getAll);
+
+//getbyId
+router.get('/blog/:id', blogController.getById);
+
+//update
+router.put('/blog', blogController.update);
+
+//delete
+router.delete('/blog/:id', blogController.delete);
+
+
+
+//comment opereations
+
+//create comment
+router.post('/comment', auth, commentController.create);
+
+//get
+router.get('/comment/:id', auth, commentController.getById);
 
 module.exports = router;
