ec4939a519d0f432b8c360e05a6b98eb0a22b7dd
Umar-Mukthar-Ahmed <21j56.umar@sjec.ac.in>
2024-12-08 08:33:53 +0530

    refractor: formated

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const {Schema} =mongoose //extracting schema constructor from mongoose
+const { Schema } = mongoose //extracting schema constructor from mongoose
 
 const commentSchema = new Schema({
-    content:{type:String, required:true},
-    blog:{type:mongoose.SchemaTypes.ObjectId, ref:'blogs'},
-    author:{type:mongoose.SchemaTypes.ObjectId, ref:'users'}
+    content: { type: String, required: true },
+    blog: { type: mongoose.SchemaTypes.ObjectId, ref: 'Blog' },
+    author: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' }
 },
-{timestamps:true}
+    { timestamps: true }
 );
 
-module.exports = mongoose.model('Comment',commentSchema,'comments');
+module.exports = mongoose.model('Comment', commentSchema, 'comments');
