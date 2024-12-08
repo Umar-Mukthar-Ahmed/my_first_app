002b8a8fd30c0951f7fb42ba2c4d039b7ab2efa8
Umar-Mukthar-Ahmed <21j56.umar@sjec.ac.in>
2024-12-08 08:33:02 +0530

    refractor: formated

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,23 +1,23 @@
-const {ValidationError} = require('joi');
+const { ValidationError } = require('joi');
 
-const errorHandler = (error,req,res,next)=>{
+const errorHandler = (error, req, res, next) => {
     //default error
     let status = 500;
-    let data={message:'Internal Server Error'};
+    let data = { message: 'Internal Server Error' };
 
-    if(error instanceof ValidationError){
+    if (error instanceof ValidationError) {
         status = 401;
-        data.message= error.message;
+        data.message = error.message;
         return res.status(status).json(data);
     }
-    
-    if(error.status){
+
+    if (error.status) {
         status = error.status;
     }
-    if(error.message){
-        data.message=error.message;
+    if (error.message) {
+        data.message = error.message;
     }
-    
+
     return res.status(status).json(data);
 };
 
