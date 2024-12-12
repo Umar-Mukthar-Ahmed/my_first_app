932d32a75a6e6d3679ef9fbc03210ddec3ad3242
Umar-Mukthar-Ahmed <21j56.umar@sjec.ac.in>
2024-12-12 10:53:31 +0530

    feat : cors added

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,20 @@ const dbConnect = require('./database/index');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            return callback(null, true);
+        },
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 
